174590fa7f3677fa11a2a8485813b8320e8b594c
chenyin <1270224500@qq.com>
2022-11-12 23:37:32 +0800

    日志模块

--- modules/nginx/nginx.py
+++ modules/nginx/nginx.py
@@ -4,3 +4,17 @@
 @Date           : 2022年 11月 08日
 @Introduction   ：A Lazy Cat
 """
+import shlex
+import subprocess
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def install_nginx():
+    cmd = "yum install -y nginx"
+    cmd = shlex.split(cmd)
+    p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out,err, = p.communicate()
+    print out,err
